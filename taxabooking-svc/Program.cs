0c5556a5a1ee36713e3cb09a8c77eeabb6dbfe48
Henrik L. Jensen <hnrk@eaaa.dk>
2022-09-27 10:56:51 +0200

    Update to OpenAPI info and Dockerfile Entrypoint.

--- taxabooking-svc/Program.cs
+++ taxabooking-svc/Program.cs
@@ -5,7 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Taxa Booking Service", Version = "v1" });
+});
 
 var app = builder.Build();
 
