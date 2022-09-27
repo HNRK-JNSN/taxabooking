b990118161dda2d3728ca829a95c7ae6711f9c5e
Henrik L. Jensen <hnrk@eaaa.dk>
2022-09-27 14:28:57 +0200

    Added new taxabooking handler service projects.

---
+++ taxabooking-handler-svc-test/TaxaBookingHandlerTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace TaxaBookingHandlerService.Test;
+
+public class TaxaBookingHandlerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
